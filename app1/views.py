d149c297004f1593ffd20331cc8543ff174e10f5
ArunRaj <arunraj753@gmail.com>
2021-09-02 08:36:32 +0530

    3rd Commit

--- app1/views.py
+++ app1/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
 	return HttpResponse('This is new home')
+
+def home2(request):
+	return HttpResponse('This is new home')
+
+def home3(request):
+	return HttpResponse('This is new home')
+
+def home4(request):
+	return HttpResponse('This is new home')
+
+def home5(request):
+	return HttpResponse('This is new home')
+
+def home6(request):
+	return HttpResponse('This is new home')
+
